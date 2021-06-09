0a9f4eed5161f7e4720b8f4e3ae5bf8c4a1b8983
Keshav Sharma <sharma.keshav39@gmail.com>
2021-06-09 23:16:05 +0530

    Add Addresses to List and Print them (#3)

--- Address.py
+++ Address.py
@@ -1,24 +1,39 @@
 class Address:
     building_name=""
-    street_town=""
+    town=""
     city=""
     zip=""
     state=""
+    addresses = []
 
+    def PrintAddresses():
+        for addr in Address.addresses:
+            print ("Your address is: " + addr.building_name + ", " + addr.town + ", " + addr.city + "-" + addr.zip + ", " + addr.state)
+    
     def AddAddress():
-        com = Address()
-        building_name = input("Enter your building name: ")
-        street_town = input("Enter your street and town: ")
-        city = input("Enter your city: ")
-        zip = input("Enter your zip: ")
-        state = input("Enter your state: ")
+        addr = Address()
+        addr.building_name = input("Enter your building name: ")
+        addr.town = input("Enter your town: ")
+        addr.city = input("Enter your city: ")
+        addr.zip = input("Enter your zip: ")
+        addr.state = input("Enter your state: ")
         
-        print ("Your address is: " + building_name + ", " + street_town + ", " + city + "-" + zip + ", " + state)
-        
+        Address.addresses.append(addr);
+
         # print("Your building name is: " + building_name)
-        # print("Your street and town is: " + street_town)
+        # print("Your town is: " + town)
         # print("Your city is: " + city)
         # print("Your zip is: " + zip)
         # print("Your state is: " + state)
 
-Address.AddAddress()
+print("1. Add Address")
+print("2. Print Addresses")
+
+while input("Should Continue?: ").lower()=="true":
+    stepNo=int(input("Enter Step No to execute:"))
+    if stepNo == 1:
+        print("Before calling AddAddress: ")
+        Address.AddAddress()
+        print("After calling AddAddress: ")
+    elif stepNo == 2:
+        Address.PrintAddresses()
