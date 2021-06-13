2369e67e63eb78a00292ad8cf05fd578a69c09a4
Keshav Sharma <sharma.keshav39@gmail.com>
2021-06-13 20:08:40 +0530

    Save Addresses To A File (#4)

--- Address.py
+++ Address.py
@@ -1,39 +1,14 @@
 class Address:
-    building_name=""
-    town=""
-    city=""
-    zip=""
-    state=""
-    addresses = []
-
-    def PrintAddresses():
-        for addr in Address.addresses:
-            print ("Your address is: " + addr.building_name + ", " + addr.town + ", " + addr.city + "-" + addr.zip + ", " + addr.state)
     
-    def AddAddress():
-        addr = Address()
-        addr.building_name = input("Enter your building name: ")
-        addr.town = input("Enter your town: ")
-        addr.city = input("Enter your city: ")
-        addr.zip = input("Enter your zip: ")
-        addr.state = input("Enter your state: ")
-        
-        Address.addresses.append(addr);
+    def __init__(self, building_name, town, city, zip, state):
+        self.building_name = building_name
+        self.town = town
+        self.city = city
+        self.zip = zip
+        self.state = state
 
-        # print("Your building name is: " + building_name)
-        # print("Your town is: " + town)
-        # print("Your city is: " + city)
-        # print("Your zip is: " + zip)
-        # print("Your state is: " + state)
+    def PrintAddress(self):
+        print ("Your address is: " + self.building_name + ", " + self.town + ", " + self.city + "-" + self.zip + ", " + self.state)
 
-print("1. Add Address")
-print("2. Print Addresses")
-
-while input("Should Continue?: ").lower()=="true":
-    stepNo=int(input("Enter Step No to execute:"))
-    if stepNo == 1:
-        print("Before calling AddAddress: ")
-        Address.AddAddress()
-        print("After calling AddAddress: ")
-    elif stepNo == 2:
-        Address.PrintAddresses()
+    def ReturnAddressAsString(self):
+        return "Your address is: " + self.building_name + ", " + self.town + ", " + self.city + "-" + self.zip + ", " + self.state
